b1ec1e9c8a8faa42bccc48fcf5184b48b4463ea5
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-04-28 23:30:43 +0200

    Add lambda_closure and Define unorthodox extended move
    The unorthodox extended move just allows for easy description of
    multiple moves to the same set of states. This commit also adds a
    function that converts a set of these extended moves to ordinary
    non-deterministic moves, in order to build an NFA.
    The unorthodox moves are used to define a test NFA taken from
    my lecture 4 notes from my automata class.

--- nfa.hs
+++ nfa.hs
@@ -30,3 +30,36 @@ instance (Show a) =>  Show (NFA a) where
 
 build_nfa :: Ord a => [a] -> [NMove a] -> a -> [a] -> NFA a
 build_nfa q delta q0 f = NFA q delta q0 f
+
+lambda_closure :: Eq a => a -> NFA a -> [a]
+lambda_closure q' (NFA q delta q0 f) = 
+    q' : concat [r | (Emove p r) <- delta, p == q']
+
+data ExtMove a = ExtMove a String [a]
+
+q :: [Int]
+q = [0 .. 5]
+delta :: [ExtMove Int]
+delta = [
+    ExtMove 0 "" [1],
+    ExtMove 0 "+-" [1],
+    ExtMove 1 "." [2],
+    ExtMove 1 "0123456789" [1, 4],
+    ExtMove 2 "0123456789" [3],
+    ExtMove 3 "" [5],
+    ExtMove 4 "." [3]
+    ]
+q0 :: Int
+q0 = 0 
+
+f :: [Int]
+f = [5]
+
+extMove_to_move :: [ExtMove a] -> [NMove a]
+extMove_to_move moves = 
+    [NMove p c q | (ExtMove p cs q) <- moves, cs /= "", c <- cs]
+    ++ 
+    [Emove p q | (ExtMove p "" q) <- moves]
+
+my_nfa :: NFA Int
+my_nfa = NFA q (extMove_to_move delta) q0 f 
