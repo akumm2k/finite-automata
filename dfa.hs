957950cc37cbd66973de7daa5e5225340088a5d6
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-05-03 01:02:17 +0200

    Add a dfa example to dfa.hs

--- dfa.hs
+++ dfa.hs
@@ -28,3 +28,23 @@ instance (Show a) =>  Show (DFA a) where
 
 build_dfa :: Ord a => [a] -> [DMove a] -> a -> [a] -> DFA a
 build_dfa q delta q0 f = DFA q delta q0 f
+
+-- L(my_dfa) = {w | w \in {0, 1}*, |w|1 % 3 == 0}
+q :: [Int]
+q = [0, 1, 2]
+
+delta :: [DMove Int]
+delta = [DMove x '0' x | x <- q] ++ [
+    DMove 0 '1' 1,
+    DMove 1 '1' 2,
+    DMove 2 '1' 0
+    ]
+
+q0 :: Int
+q0 = 0 
+
+f :: [Int]
+f = [0]
+
+my_dfa :: DFA Int
+my_dfa = DFA q delta q0 f
