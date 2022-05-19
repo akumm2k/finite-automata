0b14b4e8381be3513b68ee868b5506a2087b39ef
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-05-19 22:42:23 +0200

    Add a function to differentiate automata by states

--- Automaton.hs
+++ Automaton.hs
@@ -29,3 +29,13 @@ rmdups = map head . group . sort
 
 alphabet_of :: Automaton at => at a -> [Char]
 alphabet_of at = rmdups [c | (Move _ c _) <- moves at]
+
+differentiate_states :: (Automaton at, Show a, Eq a, Show b, Eq b) 
+    => at a -> at b -> (Int, Int, at Int, at Int)
+-- map the states to distinct ints
+-- return the lengths and new distinct automata
+differentiate_states a1 a2 = 
+    let (l1, l2) = (ls a1, ls a2) where ls = length . states 
+        a1' = isomorphism a1 [1 .. l1]
+        a2' = isomorphism a2 [l1 + 1 .. l1 + l2]
+    in (l1, l2, a1', a2')
