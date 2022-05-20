c5ac8621cdfea55afb8222a21e0084f7affac51a
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-05-20 21:37:23 +0200

    Move to sets in Automaton.hs

--- Automaton.hs
+++ Automaton.hs
@@ -1,23 +1,24 @@
 module Automaton where 
 import Data.List 
+import Data.Set
 
 data Move a = 
-    Move {from :: a, char :: Char, to :: [a]} 
-    | EMove {efrom :: a, eto :: [a]}
-    deriving (Eq)
+    Move {from :: a, char :: Char, to :: Set a} 
+    | EMove {efrom :: a, eto :: Set a}
+    deriving (Eq, Ord)
 
 class Automaton at where 
-    states :: at a -> [a]
-    start :: at a -> [a] 
-    final :: at a -> [a]
-    moves :: at a -> [(Move a)]
-    accepts :: (Eq a, Show a) => at a -> String -> Bool
-    delta :: (Eq a, Show a) => at a -> Char -> a -> [a]
-    isomorphism :: (Show a, Eq a, Show b, Eq b) => at a -> [b] -> at b
+    states :: at a -> Set a
+    start :: at a -> Set a
+    final :: at a -> Set a
+    moves :: at a -> Set (Move a)
+    accepts :: (Ord a, Show a) => at a -> String -> Bool
+    delta :: (Ord a, Show a) => at a -> Char -> a -> Set a
+    isomorphism :: (Show a, Ord a, Show b, Ord b) => at a -> Set b -> at b
 
-show_states :: Show a => [a] -> String
+show_states :: Show a => Set a -> String
 -- show_states [1, 2, 3] = "1, 2, 3"
-show_states qs = intercalate ", " (show <$> qs)
+show_states qs = intercalate ", " (show <$> (toList qs))
 
 instance (Show a) => Show (Move a) where 
     show (Move q c ps) = 
@@ -26,18 +27,18 @@ instance (Show a) => Show (Move a) where
         "(" ++ show q ++ " - " ++ "\\" ++ " -> " ++ show_states ps ++ ")"
 
 rmdups :: Ord a => [a] -> [a]
-rmdups = map head . group . sort
+rmdups = fmap head . group . sort
 
 alphabet_of :: Automaton at => at a -> [Char]
 -- return all the alphabet used in the automaton
-alphabet_of at = rmdups [c | (Move _ c _) <- moves at]
+alphabet_of at = rmdups [c | (Move _ c _) <- toList $ moves at]
 
-differentiate_states :: (Automaton at, Show a, Eq a, Show b, Eq b) 
+differentiate_states :: (Automaton at, Show a, Ord a, Show b, Ord b) 
     => at a -> at b -> (Int, Int, at Int, at Int)
 -- map the states to distinct ints
 -- return the lengths and new distinct automata
 differentiate_states a1 a2 = 
     let (l1, l2) = (ls a1, ls a2) where ls = length . states 
-        a1' = isomorphism a1 [1 .. l1]
-        a2' = isomorphism a2 [l1 + 1 .. l1 + l2]
+        a1' = isomorphism a1 (fromList [1 .. l1])
+        a2' = isomorphism a2 (fromList [l1 + 1 .. l1 + l2])
     in (l1, l2, a1', a2')
