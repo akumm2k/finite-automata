32c7ec13a39f540c95a1bc33920b6fd377178d57
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-05-20 21:38:09 +0200

    Update nfa test for sets

--- tests/nfa-test.hs
+++ tests/nfa-test.hs
@@ -1,15 +1,17 @@
 import Automaton
 import NFA
-
+import Data.Set
 {-
 Test NFA:
 accepts `[+-]?(([1-9]+\.[1-9]*)|([1-9]*\.[1-9]+))`
 -}
 
-my_q :: [Int]
-my_q = [0 .. 5]
-my_q0 :: [Int]
-my_q0 = [0] 
+
+my_q :: Set Int
+my_q = fromList [0 .. 5]
+
+my_q0 :: Set Int
+my_q0 = fromList [0] 
 
 my_delta :: [ExtMove Int]
 my_delta = [
@@ -22,11 +24,11 @@ my_delta = [
     ExtMove 4 "." [3]
     ]
 
-my_f :: [Int]
-my_f = [5]
+my_f :: Set Int
+my_f = singleton 5
 
 my_nfa :: NFA Int
 my_nfa = NFA my_q (extMove_to_move my_delta) my_q0 my_f 
 
-n :: NFA Int
-n = NFA [0 .. 3] [(EMove 0 [1]), (Move 1 '1' [2]), (EMove 2 [3])] [0] [3]
+-- n :: NFA Int
+-- n = NFA [0 .. 3] [(EMove 0 [1]), (Move 1 '1' [2]), (EMove 2 [3])] [0] [3]
