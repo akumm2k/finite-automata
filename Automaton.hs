c8dff8b22de6ba0e5ac12a71fff49b4626707c65
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-06-25 00:32:40 +0200

    Refactor Automaton.hs

--- Automaton.hs
+++ Automaton.hs
@@ -13,13 +13,11 @@ class Automaton at where
         at a -> Set b -> at b
     alphabet_of :: at a -> [Char]
 
-show_states :: Show a => Set a -> String
--- show_states [1, 2, 3] = "1, 2, 3"
-show_states qs = intercalate ", " (show <$> (toList qs))
 
-rmdups :: Ord a => [a] -> [a]
--- this is O(n log n). `nub` uses `Eq a`, so requires O(n ^ 2)
-rmdups = fmap head . group . sort
+{-
+Below are some helper functions 
+used extensively in the project
+-}
 
 differentiate_states :: 
     (Automaton at, Show a, Ord a, Show b, Ord b) 
@@ -32,15 +30,6 @@ differentiate_states a1 a2 =
         a2' = isomorphism a2 (fromList [l1 + 1 .. l1 + l2])
     in (l1, l2, a1', a2')
 
-set_null :: Set a -> Bool
-set_null = Set.null
-
-set_map :: Ord b => (a -> b) -> Set a -> Set b
-set_map = Set.map
-
-empty_set :: Set a
-empty_set = Set.empty
-
 dfs :: (Show a, Ord a, Automaton at) => 
     at a -> (at a -> String -> a -> [a]) -> String -> Set a
 -- find all reachable states in Automaton d
@@ -51,3 +40,20 @@ dfs d adjacent alphabet = fromList $ reverse $ loop s s
             let ns = adjacent d alphabet v \\ visited 
             in loop (ns ++ visited) (ns ++ vs)
         s = toList $ start d
+
+show_states :: Show a => Set a -> String
+-- show_states [1, 2, 3] = "1, 2, 3"
+show_states qs = intercalate ", " (show <$> (toList qs))
+
+rmdups :: Ord a => [a] -> [a]
+-- this is O(n log n). `nub` uses `Eq a`, so requires O(n ^ 2)
+rmdups = fmap head . group . sort
+
+set_null :: Set a -> Bool
+set_null = Set.null
+
+set_map :: Ord b => (a -> b) -> Set a -> Set b
+set_map = Set.map
+
+empty_set :: Set a
+empty_set = Set.empty
